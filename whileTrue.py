fc93ac97005484651860db664bf863a9e2977120
Brandon Wang <thebrandonwang@gmail.com>
2022-03-29 18:05:49 +0800

    first commit

---
+++ whileTrue.py
@@ -0,0 +1,10 @@
+while True:
+	mode = input('please input game mode: ')
+	if mode == '1':
+		print('game mode 1')
+	elif mode == '2':
+		print('game mode 2')
+	elif mode == 'q':
+		break
+	else:
+		print('i dont know')
